8bdebe09fefbf217189bc2d1b3954c8f6cabecc6
Shykial <szymon.jeziorski@interia.pl>
2021-06-08 17:41:43 +0200

    initial commit

---
+++ src/main/java/pl/tul/MicroserviceApplication/MicroserviceApplication.java
@@ -0,0 +1,13 @@
+package pl.tul.MicroserviceApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceApplication.class, args);
+	}
+
+}
