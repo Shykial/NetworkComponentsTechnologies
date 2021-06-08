20c402d925271d441cfd2686e33235358580ddbb
Shykial <szymon.jeziorski@interia.pl>
2021-06-08 22:49:50 +0200

    account-microservice structure fixed and finished

--- vehicle-microservice/vehicle-application-core/vehicle-application-services/src/main/java/exceptions/ServiceException.java
+++ vehicle-microservice/vehicle-application-core/vehicle-application-services/src/main/java/exceptions/ServiceException.java
@@ -5,7 +5,7 @@ public class ServiceException extends Exception {
     public static final String NULL_ARGUMENT = "One of the provided arguments is null.";
     public static final String RENT_ALREADY_ENDED = "Rent is already ended.";
     public static final String RENT_NOT_FOUND = "Rent that you are looking for was not found in the repository.";
-    public static final String NOT_AUTHORIZED = "Provided account is not autorized to perform this operation.";
+    public static final String NOT_AUTHORIZED = "Provided account is not authorized to perform this operation.";
     public static final String NOT_ENOUGH_FUNDS = "Provided client does not have enough funds to rent this car.";
     public static final String NOT_AUTHENTICATED = "Provided account was not found in the repository.";
     public static final String RENT_NOT_ENDED = "Provided rent wasn't ended yet.";
