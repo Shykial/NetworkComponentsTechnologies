4598e81b0d2f25baa8b66fa38ecf7e2a3ad4a08f
Bartłomiej Graczyk <224301@edu.p.lodz.pl>
2021-06-08 19:21:36 +0200

    Divided project for 3 microservices

---
+++ rent-microservice/src/test/java/pl/tul/rentmicroservice/RentMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.tul.rentmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ vehicle-microservice/src/main/java/pl/tul/vehiclemicroservice/VehicleMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package pl.tul.vehiclemicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleMicroserviceApplication.class, args);
+    }
+
+}
